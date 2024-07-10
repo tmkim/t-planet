82f08bb18214b2f8422775346da9388663b487a0
tmkim <taeminkim13@gmail.com>
2024-07-11 07:39:01 +0800

    fix landing page background

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 
 export default function LoginPage() {
   return (
-    <main className="flex items-center justify-center md:h-screen">
+    <main className="flex bg-slate-400 items-center justify-center md:h-screen">
       <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
         <div>
           Welcome to T-Planet!
